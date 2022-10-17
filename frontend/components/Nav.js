e19f4cb28f5078c13a2bf966d16da7e3438b1207
Mati2903 <mazurek321.dev@gmail.com>
2022-10-17 20:43:07 +0200

    Change navbar logic and style

--- frontend/components/Nav.js
+++ frontend/components/Nav.js
@@ -1,24 +1,43 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import navStyles from "../styles/Nav.module.scss";
 
 const Nav = () => {
 	const router = useRouter();
 	const [open, setOpen] = useState(false); //stan hamburgera
 
-	//funkcja do obsługi klika w menu hamburger zmieniająca stan (a przez to klasę)
+	// obsługa zmiany koloru navbara z transparent na dany kolor poprzez dodanie lub usunięcie klasy
+	// useEffect(() => {
+	// 	window.addEventListener("scroll", () => {
+	// 		if (window.pageYOffset > 70) {
+	// 			document.querySelector(".nav").classList.add("scroll");
+	// 		} else {
+	// 			document.querySelector(".nav").classList.remove("scroll");
+	// 		}
+	// 	});
+	// });
+
+	//funkcja do obsługi klika w menu hamburger zmieniająca stan (a przez to klasę) dla urządzeń mobilnych
 	const handleMenuClick = () => {
-		!open ? setOpen(true) : setOpen(false);
+		if (window.innerWidth < 768) {
+			!open ? setOpen(true) : setOpen(false);
+		}
 	};
+
+	//usuwanie klas open (zamykanie menu) po kliknięciu na stronie poza navbarem
+	useEffect(() => {
+		if (open) {
+			document.querySelector("main").addEventListener("click", () => {
+				setOpen(false);
+			});
+		}
+	}, [open]);
 
 	return (
 		<nav className="nav">
 			<p>LOGO</p>
-			<ul
-				onClick={handleMenuClick}
-				className={open ? "nav__open" : "nav__close"}
-			>
+			<ul onClick={handleMenuClick} className={`navbar ${open ? "open" : ""}`}>
 				<li className={router.pathname == "/" ? "active" : ""}>
 					<Link href="/">
 						<a>Home</a>
@@ -51,7 +70,7 @@ const Nav = () => {
 				</li>
 			</ul>
 			<div
-				className={open ? "hamburger__open" : "hamburger__close"}
+				className={`hamburger ${open ? "open" : ""}`}
 				onClick={handleMenuClick}
 			>
 				<div className="hamburger__icon"></div>
